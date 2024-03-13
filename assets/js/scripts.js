06cf9fca94e23cee41676cbe7fef8be7692b0ec5
Jorge Solis <jorge.solis.sm@gmail.com>
2024-03-12 22:01:58 -0300

    se cambia la configuracion y se arreglan detalles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,62 @@
 var puntajeusuario = 0;
-var puntajeconsola = 0;
+var puntajeComputadora = 0;
+var rondasJugadas = 0;
+var rondasTotales = 0;
+var Empates = 0;
 
-
-var opcionesconsola = ("#piedra", "#papel", "#tijeras");
-var opcionesjugador = ("#piedra", "#papel", "#tijeras");
-
-var opcionconsola = opcionesconsola(Math.floor(Math.random() * 3));
-
-document.writeln("#opcionconsola") = opcionconsola;
-document.writeln("#opcionjugador") = opcionesjugador;
-
-if (opcionesjugador === opcionesconsola) {
-    alert("Empate!");
-} else if (opcionesjugador === "#piedra" && opcionesconsola === "#tijeras" || (opcionesjugador === "#papel" && opcionesconsola === "#piedra" || opcionesjugador === "#tijeras" && opcionesconsola === "#papel")){
-    alert("Ganaste");
-    puntajeusuario++;
-} else {
-    alert("Perdiste");
-    puntajeconsola++;
+function iniciarJuego() {
+    rondasTotales = +document.getElementById('cantidadRondas').value;
+    rondasJugadas = 0;
+    actualizarRonda();
 }
 
-document.writeln("#puntajeusuario") = puntajeusuario;
-document.writeln("puntajeconsola") = puntajeconsola;
+function actualizarRonda() {
+    document.getElementById('rondaActual').innerText = rondasJugadas + 1;
+}
+
+function jugar(opcionUsuario) {
+    if (rondasJugadas >= rondasTotales) {
+        alert('Juego Terminado! Reinicia para jugar de nuevo.');
+        return;
+    }
+    var opcionesComputador = ['piedra', 'papel', 'tijera'];
+    var opcionComputadora = opcionesComputador[Math.floor(Math.random() * 3)];
+
+    document.getElementById('opcionUsuario').innerText = opcionUsuario;
+    document.getElementById('opcionComputadora').innerText = opcionComputadora;
+
+    if (opcionUsuario === opcionComputadora){
+        alert('Empate!');
+        Empates++;
+    } else if (
+        (opcionUsuario === 'piedra' && opcionComputadora === 'tijeras') ||
+        (opcionUsuario === 'papel' && opcionComputadora === 'piedra') ||
+        (opcionUsuario === 'tijeras' && opcionComputadora === 'papel')
+    ) {
+        alert('!Ganaste! Sigue asi!')
+        puntajeusuario++;
+    } else {
+        alert('Oh no, la computadora gana!')
+        puntajeComputadora++;
+    }
+rondasJugadas++;
+actualizarRonda()
+document.getElementById('puntajeUsuario').innerText = puntajeusuario;
+document.getElementById('puntajeComputadora').innerText = puntajeComputadora;
+document.getElementById('empates').innerText = Empates;
+
+if (rondasJugadas >= rondasTotales){
+    var resultadofinal;
+    if (puntajeusuario > puntajeComputadora){
+        resultadofinal = 'Felicidades, Ganaste!';
+    } else if (puntajeusuario < puntajeComputadora){
+        resultadofinal = 'Oh no, La computadora gano, intentalo otra vez!';
+    } else {
+        resultadofinal = 'Ha sido un empate!, intentalo otra vez!';
+    }
+    alert('Juego Terminado.\n' + resultadofinal + '\nPuntajefinal:\nUsuario: ' + puntajeusuario + '\nComputadora: ' + puntajeComputadora + '\nEmpates: ' + Empates)
+}
+}
+
+
+
